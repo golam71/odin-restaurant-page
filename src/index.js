070e10b0a075b382d8dcffdfe184e8d3dd862c99
golam71 <74001687+golam71@users.noreply.github.com>
2025-04-13 22:09:44 +0600

    ✨  Enhance navigation interactivity by adding border effects on section clicks and improve content clearing functionality

--- src/index.js
+++ src/index.js
@@ -8,22 +8,37 @@ import { menu } from "./menu.js";
 
 home();
 
-
 function clearContent() {
   document.getElementById("content").innerHTML = "";
+  console.log("cleared");
+}
+function clearBoarder() {
+  document.getElementById("home").style.border = "2px transparent";
+  document.getElementById("menu").style.border = "2px transparent";
+  document.getElementById("about").style.border = "2px transparent";
+}
+function addBorder(idName) {
+  document.getElementById(idName).style.border = "2px solid";
+  console.log(idName);
 }
 
 ["home", "menu", "about"].forEach((id) => {
   document.getElementById(id).addEventListener("click", () => {
     if (id === "home") {
       clearContent();
+      clearBoarder();
+      addBorder(id);
       home();
     } else if (id === "menu") {
       clearContent();
       menu();
+      clearBoarder();
+      addBorder(id);
     } else {
       clearContent();
       contact();
+      clearBoarder();
+      addBorder(id);
     }
   });
 });
